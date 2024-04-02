4c3a7869cd8a36f84e0d4c059e64057030b22b8f
garv5014 <garv5014@gmail.com>
2024-04-02 11:52:40 -0600

    I have no idea were to go with this

---
+++ src/RealTimeCommunication/Actors/SessionActors.cs
@@ -0,0 +1,13 @@
+﻿using Akka.Actor;
+
+namespace RealTimeCommunication.Actors;
+
+public class SessionActors : ReceiveActor
+{
+    public SessionActors() { }
+
+    public static Props Props()
+    {
+        return Akka.Actor.Props.Create(() => new SessionActors());
+    }
+}
