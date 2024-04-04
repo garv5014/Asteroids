1aac199946337a5700fed933b4b2e5622f3900db
garv5014 <garv5014@gmail.com>
2024-04-04 16:49:03 -0600

    LobbyActors

---
+++ src/RealTimeCommunication/Actors/Lobby/LobbyActor.cs
@@ -0,0 +1,30 @@
+﻿using Akka.Actor;
+using Akka.Event;
+
+namespace RealTimeCommunication;
+
+public class LobbyActor : ReceiveActor
+{
+  // Lobby actor is in charge of creating and managing lobbies
+  // respond to messages from the lobby supervisor after creation.
+  // Add user to lobby
+  private readonly ILoggingAdapter _log = Context.GetLogger();
+
+  public LobbyActor()
+  {
+
+  }
+  protected override void PreStart()
+  {
+    _log.Info("LobbyActor created");
+  }
+
+  protected override void PostStop()
+  {
+    _log.Info("LobbyActor stopped");
+  }
+  public static Props Props()
+  {
+    return Akka.Actor.Props.Create(() => new LobbySupervisor());
+  }
+}
---
+++ src/RealTimeCommunication/Actors/Lobby/LobbySupervisor.cs
@@ -0,0 +1,32 @@
+﻿using Akka.Actor;
+using Akka.Event;
+namespace RealTimeCommunication;
+
+public class LobbySupervisor : ReceiveActor
+{
+  // return all the lobbies lobbies stored as a list of lobbies
+  // Create a lobby actor
+  // Forward messages to the lobby actor
+  
+
+  private readonly ILoggingAdapter _log = Context.GetLogger();
+
+  public LobbySupervisor()
+  {
+    // 
+  }
+
+  protected override void PreStart()
+  {
+    _log.Info("LobbySupervisor created");
+  }
+
+  protected override void PostStop()
+  {
+    _log.Info("LobbySupervisor stopped");
+  }
+  public static Props Props()
+  {
+    return Akka.Actor.Props.Create(() => new LobbySupervisor());
+  }
+}
