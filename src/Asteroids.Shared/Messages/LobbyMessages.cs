7b6e2a2d7b526ba44115cea2e25d7fe86517cf6e
garv5014 <garv5014@gmail.com>
2024-04-05 10:53:58 -0600

    More lobby messages for creating and joining lobbies

--- src/Asteroids.Shared/Messages/LobbyMessages.cs
+++ src/Asteroids.Shared/Messages/LobbyMessages.cs
@@ -3,4 +3,8 @@
 namespace Asteroids.Shared;
 public record GameLobby(string Name, string Id, int PlayerCount);
 public record GetLobbiesMessage(string SessionActorPath) : HubMessage(null, SessionActorPath);
+public record CreateLobbyMessage(string SessionActorPath, string LobbyName) : HubMessage(null, SessionActorPath);
+public record JoinLobbyMessage(string SessionActorPath, string LobbyId) : HubMessage(null, SessionActorPath);
+public record JoinLobbyResponse(string ConnectionId, string SessionActorPath, string LobbyId) : HubMessage(ConnectionId, SessionActorPath);
+public record CreateLobbyResponse(string ConnectionId, string SessionActorPath, string LobbyId) : HubMessage(ConnectionId, SessionActorPath);
 public record AllLobbiesResponse(string ConnectionId, string SessionActorPath, GameLobby[] Lobbies) : HubMessage(ConnectionId, SessionActorPath);
