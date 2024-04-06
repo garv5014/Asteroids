157399fa60ecd915f3809a1bf2f10bb3ee599580
garv5014 <garv5014@gmail.com>
2024-04-06 12:56:07 -0600

    Waiting room

---
+++ src/Asteroids/Components/Pages/WaitingRoom.razor.cs
@@ -0,0 +1,21 @@
+﻿using Asteroids.Shared;
+
+namespace Asteroids;
+
+public partial class WaitingRoom : ILobbyClient
+{
+    public Task HandleCreateLobbyResponse(CreateLobbyResponse message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task HandleJoinLobbyResponse(JoinLobbyResponse message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task HandleLobbiesResponse(AllLobbiesResponse message)
+    {
+        throw new NotImplementedException();
+    }
+}
