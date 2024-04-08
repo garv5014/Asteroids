0b850bb6c4dd2d2bcbbc6003762b72a0c99bcd06
garv5014 <garv5014@gmail.com>
2024-04-08 14:03:56 -0600

    try this

--- src/Asteroids/Program.cs
+++ src/Asteroids/Program.cs
@@ -24,6 +24,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
 }
+app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
