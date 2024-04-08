bf269caef0b45cc81da1e1108b2609432726347f
garv5014 <garv5014@gmail.com>
2024-04-08 15:20:05 -0600

    This is a test

--- src/RealTimeCommunication/Hubs/AccountHub.cs
+++ src/RealTimeCommunication/Hubs/AccountHub.cs
@@ -12,7 +12,7 @@ public class AccountHub : Hub<IAccountClient>, IAccountHub
     private readonly IActorRef sessionSupervisor;
     private readonly ILogger<AccountHub> _logger;
     public static string UrlPath = "/accountHub";
-    public static string FullUrl = $"http://realtime:80{UrlPath}";
+    public static string FullUrl = $"http://realtime:8080{UrlPath}";
 
     public AccountHub(ILogger<AccountHub> logger, ActorRegistry actorRegistry)
     {
