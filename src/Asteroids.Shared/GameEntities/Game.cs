8047a9df4405083e142cecec2db499226d4c1f15
garv5014 <garv5014@gmail.com>
2024-04-12 11:23:59 -0600

    Works

--- src/Asteroids.Shared/GameEntities/Game.cs
+++ src/Asteroids.Shared/GameEntities/Game.cs
@@ -1,22 +1,28 @@
-using Akka.Util;
 using Microsoft.Extensions.Logging;
 
 namespace Asteroids.Shared.GameEntities;
 
 public class Game
 {
-    private readonly ILogger<Game> _log;
     private int boardHeight { get; set; }
     private int boardWidth { get; set; }
 
+    public List<((int xEdge, int yEdge), (int headingMin, int headingMax))> edges { get; set; } =
+        new List<((int xEdge, int yEdge), (int headingMin, int headingMax))>
+        {
+            ((595, 0), (0, 180)), // top
+            ((0, 595), (180, 360)), // left
+            ((595, 595), (90, 270)), // bottom
+            ((595, 0), (270, 430)) // right
+        };
+    private readonly Random _random = new();
     private Dictionary<string, Ship> ships { get; set; } = new();
     private List<Asteroid> asteroids { get; set; } = new();
 
-    public Game(int boardHeight, int boardWidth, ILogger<Game> logger)
+    public Game(int boardHeight, int boardWidth)
     {
-        boardHeight = boardHeight;
-        boardWidth = boardWidth;
-        _log = logger;
+        this.boardHeight = boardHeight;
+        this.boardWidth = boardWidth;
     }
 
     public void AddShip(string key, Ship ship)
@@ -31,12 +37,29 @@ public class Game
         CheckCollisions();
     }
 
+    public List<Ship> GetShips()
+    {
+        return ships.Values.ToList();
+    }
+
+    public List<Asteroid> GetAsteroids()
+    {
+        return asteroids;
+    }
+
+    public void UpdateShip(string key, UpdateShipParams value)
+    {
+        // Update ship movement in game state
+        if (ships.TryGetValue(key, out var ship))
+        {
+            ship.ShipMovement = value;
+        }
+    }
+
     private void MoveShips()
     {
-        _log.LogInformation("Moving ships");
         foreach (var shipEntry in ships)
         {
-            _log.LogInformation("Moving ship {0}", shipEntry.Key);
             var ship = shipEntry.Value;
 
             // Handle rotation
@@ -48,8 +71,6 @@ public class Game
             {
                 ship.Rotation += 5;
             }
-
-            ship.Rotation = (ship.Rotation + 360) % 360;
 
             if (ship.ShipMovement.IsThrusting)
             {
@@ -68,8 +89,6 @@ public class Game
 
     private void MoveAsteroids()
     {
-        _log.LogInformation("Moving asteroids");
-
         var asteroidsToRemove = new List<Asteroid>();
         foreach (var asteroid in asteroids)
         {
@@ -97,6 +116,29 @@ public class Game
             || asteroid.YCoordinate < 0
             || asteroid.XCoordinate > boardWidth
             || asteroid.XCoordinate < 0;
+    }
+
+    public void SpawnAsteroids()
+    {
+        var randomEdge = _random.Next(0, edges.Count);
+        var edge = edges[randomEdge];
+        var spawnX = _random.Next(edge.Item1.xEdge);
+        var spawnY = randomEdge == 2 ? edge.Item1.yEdge : _random.Next(edge.Item1.yEdge);
+        var size = _random.Next(20, 100);
+        var heading = _random.Next(edge.Item2.headingMin, edge.Item2.headingMax);
+        double headingRadians = Math.PI * heading / 180.0;
+        double speedFactor = 4; // Adjust this value to control asteroid speed
+
+        var asteroid = new Asteroid(
+            xCoordinate: spawnX,
+            yCoordinate: spawnY,
+            rotation: heading,
+            size: size,
+            velocityX: Math.Cos(headingRadians) * speedFactor,
+            velocityY: Math.Sin(headingRadians) * speedFactor
+        );
+
+        asteroids.Add(asteroid);
     }
 
     private void CheckCollisions()
@@ -127,5 +169,22 @@ public class Game
         {
             asteroids.Remove(asteroid);
         }
+    }
+}
+
+public static class GameExtensions
+{
+    public static LobbyState LobbyStateSnapShot(this Game game, LobbyStatus currentStatus)
+    {
+        var ships = game.GetShips();
+        var lobbyState = new LobbyState(
+            isOwner: false,
+            playerCount: ships.Count,
+            currentStatus: currentStatus,
+            ships: ships,
+            asteroids: game.GetAsteroids()
+        );
+
+        return lobbyState;
     }
 }
--- src/RealTimeCommunication/Actors/Lobby/LobbyActor.cs
+++ src/RealTimeCommunication/Actors/Lobby/LobbyActor.cs
@@ -9,14 +9,6 @@ namespace RealTimeCommunication;
 public class LobbyActor : ReceiveActor, IWithTimers
 {
     private string LobbyName { get; init; }
-    public List<((int xEdge, int yEdge), (int headingMin, int headingMax))> edges { get; set; } =
-        new List<((int xEdge, int yEdge), (int headingMin, int headingMax))>
-        {
-            ((595, 0), (0, 180)), // top
-            ((0, 595), (180, 360)), // left
-            ((595, 595), (90, 270)), // bottom
-            ((595, 0), (270, 430)) // right
-        };
     private int NumberOfPlayers
     {
         get => _sessionsToUpdate.Count;
@@ -29,18 +21,16 @@ public class LobbyActor : ReceiveActor, IWithTimers
     private string LobbyOwner { get; init; }
     public ITimerScheduler Timers { get; set; }
 
-    private LobbyState _lobbyState;
-
     private readonly ILoggingAdapter _log = Context.GetLogger();
 
     private List<IActorRef> _sessionsToUpdate = [];
-    private Dictionary<string, Ship> _ships = new();
     private readonly Random _random = new();
 
     public LobbyActor(string name, string owner)
     {
         LobbyName = name;
         LobbyOwner = owner;
+        GameState = new Game(600, 600);
         _log.Info("Lobby created with name: {0} and owner {1}", LobbyName, LobbyOwner);
         LobbyStatus = LobbyStatus.WaitingForPlayers;
         Receive<JoinLobbyMessage>(JoinLobby);
@@ -56,6 +46,7 @@ public class LobbyActor : ReceiveActor, IWithTimers
     {
         if (LobbyStatus.Equals(LobbyStatus.InGame))
         {
+            _log.Info("Game loop tick");
             GameState.Tick();
             UpdateClients();
             CheckGameOver();
@@ -64,7 +55,7 @@ public class LobbyActor : ReceiveActor, IWithTimers
 
     private void CheckGameOver()
     {
-        if (_ships.Count == 0)
+        if (GameState.GetShips().Count == 0)
         {
             _log.Info("Game over");
             LobbyStatus = LobbyStatus.GameOver;
@@ -76,13 +67,7 @@ public class LobbyActor : ReceiveActor, IWithTimers
     private void UpdateClients()
     {
         _log.Info("Updating clients");
-        List<Ship> newShips = [];
-        foreach (var ship in _ships)
-        {
-            newShips.Add(ship.Value);
-        }
 
-        _lobbyState.Ships = newShips;
         foreach (var session in _sessionsToUpdate)
         {
             _log.Info("Sending lobby state to {0}", session.Path);
@@ -90,7 +75,7 @@ public class LobbyActor : ReceiveActor, IWithTimers
                 new LobbyStateResponse(
                     ConnectionId: "",
                     SessionActorPath: session.Path.ToString(),
-                    CurrentState: _lobbyState
+                    CurrentState: GameState.LobbyStateSnapShot(LobbyStatus)
                 )
             );
         }
@@ -98,27 +83,7 @@ public class LobbyActor : ReceiveActor, IWithTimers
 
     private void SpawnAsteroids(SpawnAsteroidMessage obj)
     {
-        _log.Info("Spawning asteroids");
-
-        var randomEdge = _random.Next(0, edges.Count);
-        var edge = edges[randomEdge];
-        var spawnX = _random.Next(edge.Item1.xEdge);
-        var spawnY = randomEdge == 2 ? edge.Item1.yEdge : _random.Next(edge.Item1.yEdge);
-        var size = _random.Next(20, 100);
-        var heading = _random.Next(edge.Item2.headingMin, edge.Item2.headingMax);
-        double headingRadians = Math.PI * heading / 180.0;
-        double speedFactor = 4; // Adjust this value to control asteroid speed
-
-        var asteroid = new Asteroid(
-            xCoordinate: spawnX,
-            yCoordinate: spawnY,
-            rotation: heading,
-            size: size,
-            velocityX: Math.Cos(headingRadians) * speedFactor,
-            velocityY: Math.Sin(headingRadians) * speedFactor
-        );
-
-        _lobbyState.Asteroids.Add(asteroid);
+        GameState.SpawnAsteroids();
         ScheduleNextAsteroidSpawn();
     }
 
@@ -132,122 +97,13 @@ public class LobbyActor : ReceiveActor, IWithTimers
         );
     }
 
-    // Call this method in the constructor or appropriate place to start the asteroid spawning
-
-    private void MoveAsteroids()
-    {
-        _log.Info("Moving asteroids");
-
-        var asteroidsToRemove = new List<Asteroid>();
-        foreach (var asteroid in _lobbyState.Asteroids)
-        {
-            if (
-                asteroid.YCoordinate > _lobbyState.boardHeight
-                || asteroid.YCoordinate < 0
-                || asteroid.XCoordinate > _lobbyState.boardWidth
-                || asteroid.XCoordinate < 0
-            )
-            {
-                asteroidsToRemove.Add(asteroid);
-            }
-            else
-            {
-                asteroid.XCoordinate += (int)asteroid.VelocityX;
-                asteroid.YCoordinate += (int)asteroid.VelocityY;
-            }
-        }
-
-        foreach (var asteroid in asteroidsToRemove)
-        {
-            _lobbyState.Asteroids.Remove(asteroid);
-        }
-    }
-
-    private void MoveShips()
-    {
-        _log.Info("Moving ships");
-        foreach (var shipEntry in _ships)
-        {
-            _log.Info("Moving ship {0}", shipEntry.Key);
-            var ship = shipEntry.Value;
-
-            if (ship.ShipMovement.IsRotatingRight)
-            {
-                ship.Rotation -= 10;
-            }
-            else if (ship.ShipMovement.IsRotatingLeft)
-            {
-                ship.Rotation += 10;
-            }
-
-            ship.Rotation = (ship.Rotation + 360) % 360;
-
-            if (ship.ShipMovement.IsThrusting)
-            {
-                double radians = Math.PI * ship.Rotation / 180.0;
-                ship.VelocityX += Math.Cos(radians) * 0.1;
-                ship.VelocityY += Math.Sin(radians) * 0.1;
-            }
-
-            // Update ship position based on velocity
-            ship.XCoordinate += (int)ship.VelocityX;
-            ship.YCoordinate += (int)ship.VelocityY;
-
-            // Screen wrapping
-            ship.XCoordinate = (ship.XCoordinate + _lobbyState.boardWidth) % _lobbyState.boardWidth;
-            ship.YCoordinate =
-                (ship.YCoordinate + _lobbyState.boardHeight) % _lobbyState.boardHeight;
-        }
-    }
-
-    private void CheckCollisions()
-    {
-        var asteroidsToRemove = new List<Asteroid>();
-        foreach (var asteroid in _lobbyState.Asteroids)
-        {
-            foreach (var shipEntry in _ships)
-            {
-                var ship = shipEntry.Value;
-                // Calculate distance between ship and asteroid
-                double distance = Math.Sqrt(
-                    Math.Pow(asteroid.XCoordinate - ship.XCoordinate, 2)
-                        + Math.Pow(asteroid.YCoordinate - ship.YCoordinate, 2)
-                );
-
-                // Check if distance is less than sum of their radii (size / 2 for simplicity)
-                if (distance < (asteroid.Size / 2) + (ship.Size / 2))
-                {
-                    ship.Health -= 10;
-                    asteroidsToRemove.Add(asteroid);
-
-                    // Optional: Add logic to handle what happens when health reaches 0
-                    if (ship.Health <= 0)
-                    {
-                        // Ship destroyed
-                        _ships.Remove(shipEntry.Key);
-                    }
-
-                    // Optional: Remove asteroid or split into smaller asteroids
-                }
-            }
-        }
-        foreach (var asteroid in asteroidsToRemove)
-        {
-            _lobbyState.Asteroids.Remove(asteroid);
-        }
-    }
-
     private void UpdateShip(UpdateShipMessage obj)
     {
         if (LobbyStatus != LobbyStatus.InGame)
             return;
 
         // Update ship movement in game state
-        if (_ships.TryGetValue(obj.SessionActorPath, out var ship))
-        {
-            _log.Info("Updating ship for {0} in LobbyActor", obj.SessionActorPath);
-            ship.ShipMovement = obj.ShipParams;
-        }
+        GameState.UpdateShip(obj.SessionActorPath, obj.ShipParams);
     }
 
     private void UpdateLobby(UpdateLobbyMessage msg)
@@ -264,19 +120,11 @@ public class LobbyActor : ReceiveActor, IWithTimers
 
         if (msg.NewStatus == LobbyStatus.InGame)
         {
-            _lobbyState = new LobbyState(
-                isOwner: LobbyOwner == msg.SessionActorPath,
-                playerCount: NumberOfPlayers,
-                currentStatus: LobbyStatus,
-                ships: _ships.Values.ToList(),
-                asteroids: []
-            );
-
             Context.Parent.Tell(
                 new LobbyStateResponse(
                     ConnectionId: msg.ConnectionId,
                     SessionActorPath: msg.SessionActorPath,
-                    CurrentState: _lobbyState
+                    CurrentState: GameState.LobbyStateSnapShot(LobbyStatus)
                 )
             );
 
@@ -325,7 +173,10 @@ public class LobbyActor : ReceiveActor, IWithTimers
     private void JoinLobby(JoinLobbyMessage msg)
     {
         _sessionsToUpdate.Add(Sender); // should be the session Actor.
-        _ships[msg.SessionActorPath] = new Ship(xCoordinate: 0, yCoordinate: 0, rotation: 0);
+        GameState.AddShip(
+            msg.SessionActorPath,
+            new Ship(xCoordinate: 0, yCoordinate: 0, rotation: 0)
+        );
         _log.Info("Lobby Joined by {0}", Sender.Path.Name);
     }
 
