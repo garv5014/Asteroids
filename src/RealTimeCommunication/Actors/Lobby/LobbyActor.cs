80bd8b6cda774169418fce420ecaae1648a2aba9
garv5014 <garv5014@gmail.com>
2024-04-18 11:24:00 -0600

    Add ErrorHubUrl to SignalREnv options and send error message in LobbyActor

--- src/RealTimeCommunication/Actors/Lobby/LobbyActor.cs
+++ src/RealTimeCommunication/Actors/Lobby/LobbyActor.cs
@@ -194,6 +194,7 @@ public class LobbyActor : ReceiveActor, IWithTimers
     protected override void PreStart()
     {
         _log.Info("LobbyActor created");
+        Context.Parent.Tell(new ErrorMessage("Made new error"));
     }
 
     protected override void PostStop()
